08141d54200bbcc75f70e83ae7867ae761455682
maeda-m <maeda-m@matsukei.co.jp>
2015-07-14 22:01:36 +0900

    Implemented replace email subject of notification

--- app/models/project_notification_setting.rb
+++ app/models/project_notification_setting.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class ProjectNotificationSetting < ActiveRecord::Base
   unloadable
 
@@ -7,4 +8,29 @@ class ProjectNotificationSetting < ActiveRecord::Base
   safe_attributes 'project_id', 'project_name'
 
   validates :project_id, uniqueness: true, presence: true
+
+  def replace_subject(subject)
+    subject.sub!(project.name, project_name) if project_name.present?
+
+    subject
+  end
+
+  class << self
+
+    def replace_headers(global_headers, local_headers)
+      # Mailer.headers['X-Redmine-Project'] #=> Mail::Field
+      project_identifier = global_headers['X-Redmine-Project'].to_s
+      return local_headers if project_identifier.blank?
+
+      project = Project.where(identifier: project_identifier).first
+      notification_setting = project.try(:notification_setting)
+
+      if project.present? && notification_setting.present?
+        local_headers[:subject] = notification_setting.replace_subject(local_headers[:subject])
+      end
+
+      return local_headers
+    end
+
+  end
 end
---
+++ app/patches/mailer_patch.rb
@@ -0,0 +1,21 @@
+require_dependency 'mailer'
+
+module CustomNotification
+  module MailerPatch
+    unloadable
+
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method :mail_without_custom_notification, :mail
+
+      def mail(local_headers = {}, &block)
+        local_headers = ProjectNotificationSetting.replace_headers(self.headers, local_headers)
+        mail_without_custom_notification(local_headers, &block)
+      end
+    end
+
+  end
+end
+
+Mailer.send :include, CustomNotification::MailerPatch
